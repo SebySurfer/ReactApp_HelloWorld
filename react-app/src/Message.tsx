6f5517b8919dc67010719660d90a7402f86ea827
SebySurfer <sebysurfer@gmail.com>
2024-09-16 14:04:24 -0600

    Basics

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,5 +1,19 @@
 
-//PascalCasing = optional convention for devs to follow, Capitalizing the first letter 
+//PascalCasing = onlig for React, optional convention for devs to follow, Capitalizing the first letter 
+
+//Message = child component of App (parent)
 function Message(){
 
+    const name = '';
+
+    if(name){
+        //JSX: JavaScript XML
+        return <h1>Hello {name}</h1>
+    } else {
+        return <h1>Hello World</h1>
+    }
+   
 }
+
+export default Message;
+
